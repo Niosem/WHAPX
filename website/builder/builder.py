e24f025721590bd39889c1ebfeb777c412b3394c
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-09 21:04:18 +0100

    au

--- website/builder/builder.py
+++ website/builder/builder.py
@@ -9,6 +9,6 @@ string charLabel0 = "cryx0"
 def gr_update () : {
     gamelib.createGameWindow(x=800, y=600);
     gamelib.GRiS(grisLybel, xx=2423);
-    gamelib.loadModdedBufffer(memAddr = 0xD92A);
+    gamelib.loadModdedBufffer(memAddr = 0xD92A, mod=charLabel0);
     gamelib.PRiS(prisLybel, yy=2093);
 }
