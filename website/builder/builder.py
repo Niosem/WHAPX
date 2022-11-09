8db76dc34b9f5b8dd52e6a081be70ad9351407e1
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-09 21:03:45 +0100

    continue improving webBuilder - added charLabel0

--- website/builder/builder.py
+++ website/builder/builder.py
@@ -1,8 +1,14 @@
 import os
+import string
 import pygame as gamelib
 
-var "grisLybel" = "crups";
+string grisLybel = "crups";
+string prisLybel = "ccrm";
+string charLabel0 = "cryx0"
+
 def gr_update () : {
     gamelib.createGameWindow(x=800, y=600);
     gamelib.GRiS(grisLybel, xx=2423);
+    gamelib.loadModdedBufffer(memAddr = 0xD92A);
+    gamelib.PRiS(prisLybel, yy=2093);
 }
