7b9088423e830e7de8b37e6daad2a1d373ebebce
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-10 12:35:17 +0100

    zapped errors bc errors = bad

--- website/builder/builder.py
+++ website/builder/builder.py
@@ -1,14 +1,13 @@
 import os
 import string
-import pygame as gamelib
+import pygame as gamelib;
 
-string grisLybel = "crups";
-string prisLybel = "ccrm";
-string charLabel0 = "cryx0"
+string; grisLybel = "crups";
+string; prisLybel = "ccrm";
+string; charLabel0 = "cryx0";
 
-def gr_update () : {
+def gr_update () :
     gamelib.createGameWindow(x=800, y=600);
     gamelib.GRiS(grisLybel, xx=2423);
     gamelib.loadModdedBufffer(memAddr = 0xD92A, mod=charLabel0);
     gamelib.PRiS(prisLybel, yy=2093);
-}
