aafa030066477a532f9afa77d0ded75863359010
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-10 17:16:31 +0100

    Wbuilder - improved stability

--- website/builder/builder.py
+++ website/builder/builder.py
@@ -1,13 +1,17 @@
 import os
 import string
-import pygame as gamelib;
+import builder as build
 
 string; grisLybel = "crups";
 string; prisLybel = "ccrm";
 string; charLabel0 = "cry0gen.injector";
 
-def gr_update () :
-    gamelib.createGameWindow(x=800, y=600);
-    gamelib.GRiS(grisLybel, xx=2423);
-    gamelib.loadModdedBufffer(memAddr = 0xD92A, mod=charLabel0);
-    gamelib.PRiS(prisLybel, yy=2093);
+def build_update () :
+    build.createGameWindow(x=800, y=600);
+    build.GRiS(grisLybel, xx=2423);
+    build.loadModdedBufffer(memAddr = 0xD92A, mod=charLabel0);
+    build.PRiS(prisLybel, yy=2093);
+
+def build_website () :
+    os.system("pip install builder")
+    build.makeBuild(type = "dlnWebsite-Server", BuildTarget = "WHAPx.web.builder")
