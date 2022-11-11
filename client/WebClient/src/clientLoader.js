fd21976e60872f3812d7e5707674ecc7ae61fd86
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-11 11:09:20 +0100

    Improved clientLoader

--- client/WebClient/src/clientLoader.js
+++ client/WebClient/src/clientLoader.js
@@ -1,6 +1,18 @@
 function Load_PreloadEnvironement(qrt = true) {
-    env.load(PreLoadWolf, version = "0.0.01", filePath = "file:///localhost/whapx/env/preloader.env.tar.gz")
-    archive.expand(archivePath = "localhost/whapx/env/preloader.env.tar.gz", uzPath = "file:///localhost/tmp/pr.tar")
-    archive.untar(tarPath = "file:///localhost/tmp/pr.tar", utPath = "file:///localhost/whapx/env/preloader.env")
-    file.delete(filePath = "file:///localhost/tmp/pr.tar")
+    function Expand_Preload(qrt = true) {
+        env.load(PreLoadWolf, version = "0.0.01", filePath = "file:///localhost/whapx/env/preloader_env.tar.gz")
+        archive.expand(archivePath = "localhost/whapx/env/preloader_env.tar.gz", uzPath = "file:///localhost/tmp/pr.tar")
+        archive.untar(tarPath = "file:///localhost/tmp/pr.tar", utPath = "file:///localhost/whapx/env/preloader_env")
+        file.delete(filePath = "file:///localhost/tmp/pr.tar")}
+    
+    Expand_Preload()
+
+    function Preload_Start() {
+        directory.change(path = "file:///localhost/whapx/env/preloader_env")
+        shell.command("./pr_start.sh")
+    }
+
+    Preload_Start()
 }
+
+Load_PreloadEnvironement()
