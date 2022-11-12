0f6bb67bac710f97b138824e38b41951b030260f
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-12 17:05:37 +0100

    added apache2 c headers

--- client/secudesk/main.c
+++ client/secudesk/main.c
@@ -1,8 +1,34 @@
 //Libraries
     #include <stdio.h>
     #include "./src/api-controller.h"
+    #include <regex.h>
 
-    //Security
+    //unicode
+        #include <unicode/localpointer.h>
+        #include <unicode/parseerr.h>
+        #include <unicode/platform.h>
+        #include <unicode/ptypes.h>
+        #include <unicode/putil.h>
+        #include <unicode/stringoptions.h>
+        #include <unicode/uchar.h>
+        #include <unicode/uconfig.h>
+        #include <unicode/ucpmap.h>
+        #include <unicode/uidna.h>
+        #include <unicode/uiter.h>
+        #include <unicode/umachine.h>
+        #include <unicode/uregex.h>
+        #include <unicode/urename.h>
+        #include <unicode/ustring.h>
+        #include <unicode/utext.h>
+        #include <unicode/utf.h>
+        #include <unicode/utf16.h>
+        #include <unicode/utf8.h>
+        #include <unicode/utf_old.h>
+        #include <unicode/utypes.h>
+        #include <unicode/uvernum.h>
+        #include <unicode/uversion.h>
+
+    //Security / Secure
         #include <security/openpam.h>
         #include <security/openpam_attr.h>
         #include <security/openpam_version.h>
@@ -10,10 +36,57 @@
         #include <security/pam_constants.h>
         #include <security/pam_modules.h>
         #include <security/pam_types.h>
+        #include <secure/_common.h>
 
         //Audit
             #include <security/audit/audit_ioctl.h>
 
+    //Apache2
+    #include <apache2/apache_noprobes.h>
+    #include <apache2/ap_compat.h>
+    #include <apache2/ap_config.h>
+    #include <apache2/ap_config_auto.h>
+    #include <apache2/ap_config_layout.h>
+    #include <apache2/ap_expr.h>
+    #include <apache2/ap_hooks.h>
+    #include <apache2/ap_listen.h>
+    #include <apache2/ap_mmn.h>
+    #include <apache2/ap_mpm.h>
+    #include <apache2/ap_provider.h>
+    #include <apache2/ap_regex.h>
+    #include <apache2/ap_regkey.h>
+    #include <apache2/ap_regkey.h>
+    #include <apache2/ap_slotmem.h>
+    #include <apache2/ap_socache.h>
+    #include <apache2/cache_common.h>
+    #include <apache2/heartbeat.h>
+    #include <apache2/httpd.h>
+    #include <apache2/http_config.h>
+    #include <apache2/http_connection.h>
+    #include <apache2/http_core.h>
+    #include <apache2/http_log.h>
+    #include <apache2/http_main.h>
+    #include <apache2/http_protocol.h>
+    #include <apache2/http_request.h>
+    #include <apache2/http_ssl.h>
+    #include <apache2/http_vhost.h>
+    #include <apache2/modperl_apache_compat.h>
+    #include <apache2/modperl_apache_includes.h>
+    #include <apache2/modperl_apr_compat.h>
+    #include <apache2/modperl_apr_includes.h>
+    #include <apache2/modperl_apr_perlio.h>
+    #include <apache2/modperl_bucket.h>
+    #include <apache2/modperl_callback.h>
+    #include <apache2/modperl_cgi.h>
+    #include <apache2/modperl_cmd.h>
+    #include <apache2/modperl_common_includes.h>
+    #include <apache2/modperl_common_log.h>
+    #include <apache2/modperl_common_types.h>
+    #include <apache2/modperl_common_util.h>
+    #include <apache2/modperl_config.h>
+    #include <apache2/modperl_const.h>
+    #include <apache2/modperl_constants.h>
+    #include <apache2/modperl_debug.h>
 
 //MemoryDefine
 
